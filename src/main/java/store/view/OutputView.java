b5bc24e4484b31f6a2bdda51326f35cdefaa0055
howareyoo <newworld121@naver.com>
2024-11-10 15:57:35 +0900

    feat: add current produsts list print method

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,21 @@
+package store.view;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import store.model.Product;
+import store.repository.ProductRepository;
+
+public class OutputView {
+
+    public void printProducts() throws URISyntaxException {
+        System.out.println("안녕하세요. W편의점입니다.\n"
+                + "현재 보유하고 있는 상품입니다.");
+        String fileName = "products.md";
+        ProductRepository productDAO = new ProductRepository(fileName);
+
+        List<Product> products = productDAO.getProducts();
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+}
