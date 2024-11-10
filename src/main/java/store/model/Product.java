757fa469b71f14d99187cce096d989dd2c2e4b49
howareyoo <newworld121@naver.com>
2024-11-10 15:55:42 +0900

    feat: add product display method
    when product quantity 0, when promotion no exist

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -3,21 +3,35 @@ package store.model;
 public class Product {
     private String name;
     private int price;
-    private int quantity;
+    private String quantity;
     private String promotion;
 
-    public Product(String name, int price, int quantity, String promotion) {
+    public Product(String name, int price, String quantity, String promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.promotion = promotion;
     }
 
+    private String getQuantityDisplay() {
+        if (quantity.equals("0")) {
+            return "재고 없음";
+        }
+        return quantity + "개";
+    }
+
+    private String getPromotionDisplay() {
+        if (promotion.equals("null")) {
+            return "";
+        }
+        return promotion;
+    }
+
     @Override
     public String toString() {
         String priceFommat = String.format("%,d원", price);
-        return String.join(" ", name, priceFommat, quantity + "개", promotion);
+        String quantityDisplay = getQuantityDisplay();
+        String promotionDisplay = getPromotionDisplay();
+        return String.join(" ", "-", name, priceFommat, quantityDisplay, promotionDisplay);
     }
-
-
 }
