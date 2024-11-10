e370ed32e7705b849a4a1f52f24f1939ea144eb1
howareyoo <newworld121@naver.com>
2024-11-10 11:42:42 +0900

    feat: add input view membership apply

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,4 +7,9 @@ public class InputView {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
+
+    public String membershipApply(){
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
 }
