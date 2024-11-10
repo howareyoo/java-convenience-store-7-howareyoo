b8ea1064435c4e9265d1ba33ebc3f9ae775a7653
howareyoo <newworld121@naver.com>
2024-11-10 12:22:54 +0900

    feat: add input view additional purchases

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -37,4 +37,9 @@ public class InputView {
         return Console.readLine();
     }
 
+    public String readAdditionalPurchases() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        return Console.readLine();
+    }
+
 }
