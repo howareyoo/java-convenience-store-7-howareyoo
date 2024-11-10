d6475a86134553ffda675c58a2a1e722f6fc41f5
howareyoo <newworld121@naver.com>
2024-11-10 21:29:29 +0900

    feat: dd promotion-based stock update in checkPromotionQuantity method

--- src/main/java/store/service/ProductService.java
+++ src/main/java/store/service/ProductService.java
@@ -6,13 +6,28 @@ import store.repository.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final PromotionService promotionService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, PromotionService promotionService) {
         this.productRepository = productRepository;
+        this.promotionService = promotionService;
     }
 
-    public boolean isProductExist(String productName) throws URISyntaxException {
+    public boolean isProductExist(String productName) {
         return productRepository.getProducts().stream().anyMatch(product -> product.hasName(productName));
+    }
+
+    public void checkPromotionQuantity(String productName, String promotionName, int userPurchaseQuantity)
+            throws URISyntaxException {
+        boolean promotionApply = promotionService.isPromotionApply(productName, userPurchaseQuantity);
+
+        if (promotionApply) {
+            productRepository.updatePromotionQuantity(productName, promotionName, userPurchaseQuantity);
+        }
+
+        if (!promotionApply) {
+            productRepository.updateProductQuantity(productName, userPurchaseQuantity);
+        }
     }
 
 }
