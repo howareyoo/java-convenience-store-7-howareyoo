a80e294005daa59a6e1b8740dce8b13fb275fbf1
howareyoo <newworld121@naver.com>
2024-11-10 18:52:07 +0900

    feat: add promotion eligibility and additional purchase check

---
+++ src/main/java/store/service/PromotionService.java
@@ -0,0 +1,28 @@
+package store.service;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import store.model.Promotion;
+import store.repository.PromotionRepository;
+
+public class PromotionService {
+
+    private final PromotionRepository promotionRepository;
+    private final String fineName = "promotions.md";
+
+    public PromotionService(PromotionRepository promotionRepository) {
+        this.promotionRepository = promotionRepository;
+    }
+
+    public boolean isPromotionApply(String promotionName, int userPurchaseQuantity) throws URISyntaxException {
+        List<Promotion> promotions = promotionRepository.getPromotions();
+        return promotionRepository.findPromotion(promotionName, promotions)
+                .map(promotion -> promotion.applyPromotion(userPurchaseQuantity)).orElse(false);
+    }
+
+    public int getAddQuantityNeed(String promotionName, int userPurchaseQuantity) throws URISyntaxException {
+        List<Promotion> promotions = promotionRepository.getPromotions();
+        return promotionRepository.findPromotion(promotionName, promotions)
+                .map(promotion -> promotion.getBuyQuantity(userPurchaseQuantity)).orElse(0);
+    }
+}
