da9cf2a3fe5e374a313f232bac286a2bcd76c9ba
howareyoo <newworld121@naver.com>
2024-11-11 01:19:29 +0900

    refactor: add mebership discount boolean variable

--- src/main/java/store/service/MembershipService.java
+++ src/main/java/store/service/MembershipService.java
@@ -4,16 +4,19 @@ import store.contents.MembershipDiscount;
 
 public class MembershipService {
 
-    public int applayMembershipDiscount(int notApplicablePrice) {
-        float discountRate = MembershipDiscount.MEMBERSHIP_DISCOUNT.getDiscountRate();
-        int maxDiscount = MembershipDiscount.MEMBERSHIP_DISCOUNT.getMaxDiscount();
+    public int applayMembershipDiscount(boolean isApply, int notApplicablePrice) {
+        if (isApply) {
+            float discountRate = MembershipDiscount.MEMBERSHIP_DISCOUNT.getDiscountRate();
+            int maxDiscount = MembershipDiscount.MEMBERSHIP_DISCOUNT.getMaxDiscount();
 
-        int discount = (int) (notApplicablePrice * discountRate);
-        
-        if (discount > maxDiscount) {
-            discount = maxDiscount;
+            int discount = (int) (notApplicablePrice * discountRate);
+
+            if (discount > maxDiscount) {
+                discount = maxDiscount;
+            }
+            return discount;
         }
 
-        return discount;
+        return 0;
     }
 }
